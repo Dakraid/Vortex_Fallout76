55dc8f2df52047099b90e4595b141ec7f11e3710
mxswat <massimocacciapaglia.13@gmail.com>
2021-09-01 13:07:45 +0100

    feat: updated the way fallout76Custom is edited

--- index.js
+++ index.js
@@ -25,17 +25,17 @@ const tools = [
 
 function findGame() {
     return util.GameStoreHelper.findByAppId(STEAM_APP_ID)
-    .then((game) => {
-        return game.path;
-    })
-    .catch(() => {
-        // Try finding the game on Bethesda.net if the Steam detection fails.
-        const bethNetPath = winapi.RegGetValue('HKEY_LOCAL_MACHINE',
-        'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Fallout 76',
-        'Path');
-        if (!bethNetPath) throw new Error('empty registry key');
-        return Promise.resolve(instPath.value);
-    });
+        .then((game) => {
+            return game.path;
+        })
+        .catch(() => {
+            // Try finding the game on Bethesda.net if the Steam detection fails.
+            const bethNetPath = winapi.RegGetValue('HKEY_LOCAL_MACHINE',
+                'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Fallout 76',
+                'Path');
+            if (!bethNetPath) throw new Error('empty registry key');
+            return Promise.resolve(instPath.value);
+        });
 }
 
 function onGameModeActivated(gameId, api) {
@@ -45,34 +45,34 @@ function onGameModeActivated(gameId, api) {
 
     // Make sure the folder in My Documents exists, create it if not. 
     return fs.ensureDirAsync(iniPath)
-    .then(() => {
-        // See if our INI exists
-        fs.statAsync(ini)
-        .then(() => ini)
-        .catch(err => {
-            // If the INI doesn't exist, make one.
-            if (err.code === 'ENOENT') return createINI(ini, api);
-            // report any other errors.
-            else api.sendNotification({id: 'fallout76-ini-error', type: 'error', title: 'Error reading Fallout76Custom.ini', message: `${err.code} - ${err.message}`});
+        .then(() => {
+            // See if our INI exists
+            fs.statAsync(ini)
+                .then(() => ini)
+                .catch(err => {
+                    // If the INI doesn't exist, make one.
+                    if (err.code === 'ENOENT') return createINI(ini, api);
+                    // report any other errors.
+                    else api.sendNotification({ id: 'fallout76-ini-error', type: 'error', title: 'Error reading Fallout76Custom.ini', message: `${err.code} - ${err.message}` });
+                })
         })
-    })
-    .catch((err) => console.log('Error checking INI folder', err))
+        .catch((err) => console.log('Error checking INI folder', err))
 
 }
 
 function createINI(iniLocation, api) {
     // Creates a new Fallout76Custom.ini with the default settings
-    const defaultData = 
+    const defaultData =
         '[Archive]\n' +
         'sResourceArchive2List=\n' +
         'sResourceDataDirsFinal=STRINGS\\\n' +
         'bInvalidateOlderFiles=1\n';
 
     return fs.writeFileAsync(iniLocation, defaultData).then(() => iniLocation)
-    .catch((err) => {
-        api.sendNotification({id:'fallout76-ini-create-error', type: 'error', title: 'Could not create Fallout76Custom.ini', message: `${err.code} - ${err.message}`});
-        return '';
-    });
+        .catch((err) => {
+            api.sendNotification({ id: 'fallout76-ini-create-error', type: 'error', title: 'Could not create Fallout76Custom.ini', message: `${err.code} - ${err.message}` });
+            return '';
+        });
 }
 
 function testForArchives(files, gameId) {
@@ -91,25 +91,21 @@ function installAndRegisterArchives(files) {
     // Get an array of the file names of the ba2 files (in case they're nested for some reason)    
     const ba2Files = files.filter(f => path.extname(f).toLowerCase() === ARCHIVE_EXT).map(a => path.basename(a));
     // Add this attribute to the mod when we install it. 
-    if (ba2Files.length) instructions.push({type: 'attribute', key: 'ba2archives', value: ba2Files});
+    if (ba2Files.length) instructions.push({ type: 'attribute', key: 'ba2archives', value: ba2Files });
 
     // A basic install pattern just to ensure the files are still copied over. 
-    const fileInstructions = files.map(f => { return {type: 'copy', source: f, destination: f}});
+    const fileInstructions = files.map(f => { return { type: 'copy', source: f, destination: f } });
 
     // Combine the attributes and instructions, then return.
     instructions = instructions.concat(fileInstructions);
     return Promise.resolve({ instructions });
 }
 
-function updateArchiveList(profileId, api) {
+function updateArchiveList(profile, api) {
     // Get the current profile and check if it's for Fallout 76
-    const profile = selectors.profileById(api.store.getState(), profileId);
-    const gameId = profile.gameId;
-    if (gameId !== GAME_ID) return;
 
-    
     const state = api.store.getState();
-
+    const gameId = profile.gameId;
     // Filter through the enabled mods that have BA2 archives
     const activeBA2Mods = Object.keys(profile.modState).map(key => {
         if (!profile.modState[key].enabled) return;
@@ -126,31 +122,49 @@ function updateArchiveList(profileId, api) {
     const dataFolder = path.join(gamePath, 'Data');
     const fallout76Custom = path.join(iniPath, fallout76CustomINI);
 
-    return fs.readdirAsync(dataFolder)
-    .then((dataFiles) => {
-        // Get a list of all BA2s in the data folder.
-        const dataFolderBA2s = dataFiles.filter(f => path.extname(f) === ARCHIVE_EXT);
-        // Read our INI
-        return parser.read(fallout76Custom)
+    return parser.read(fallout76Custom)
         .then((ini) => {
-            // Get the current INI list and convert it to an array.
-            const iniList = ini.data.Archive.sResourceArchive2List;
-            const iniArray = iniList.length ? iniList.split(',').map(b => b.trim()) : [];
-            // Filter out any missing ba2s
-            let newArchives = iniArray.filter(b => dataFolderBA2s.includes(b));
-            // Add any new BA2s that don't exist. 
-            newArchives = newArchives.concat(activeBA2s.filter(b => dataFolderBA2s.includes(b) && !newArchives.includes(b))).join(', ');
-            // If the two lists don't match, update them. 
-            if (newArchives !== iniList) {
-                ini.data.Archive.sResourceArchive2List = newArchives;
-                return parser.write(fallout76Custom, ini).then(() => Promise.resolve())
+            // const iniList = ini.data.Archive.sResourceArchive2List;
+            // Also assume that the user is using only Vortex to handle the `sResourceArchive2List` lists
+            // And assume vortex is deploying correctly Since going through the Data folder and original config seems unnecessary
+            const cleanedArchivesList = [...new Set([...activeBA2s])].filter(function (el) {
+                return !!el; // Removing empty strings
+            });
+            ini.data.Archive.sResourceArchive2List = cleanedArchivesList
+            return parser.write(fallout76Custom, ini).then(() => Promise.resolve())
                 .catch(err => log('error', 'Error updating Fallout76Custom.ini', err));
-            }
-            return Promise.resolve();
         })
         .catch(err => log('error', 'Error parsing Fallout76Custom.ini', err));
-    })
-    .catch(err => log('error', 'Error reading data folder for Fallout 76', err));
+}
+
+
+/**
+ * Using the onStateChange props, this function checks for the profile that has changed and returns it
+ * @param {*} previous 
+ * @param {*} current 
+ * @returns the changed profile
+ */
+function getChangedProfile(previous, current) {
+    if ((previous === undefined) || (current === undefined)) {
+        return;
+    }
+    const profileIds = Object.keys(previous)
+    for (let i = 0; i < profileIds.length; i++) {
+        const profileId = profileIds[i];
+        if ((previous[profileId] !== current[profileId]) && (current[profileId] !== undefined)) {
+            return current[profileId]
+        }
+    }
+}
+
+let debouceUpdate = setTimeout(() => { }, 0);
+
+function deboucedUpdateArchiveList(previous, current, api) {
+    const changedProfile = getChangedProfile(previous, current)
+    if (!changedProfile) {
+        return
+    }
+    updateArchiveList(changedProfile, api)
 }
 
 function main(context) {
@@ -186,8 +200,14 @@ function main(context) {
         // When we activate Fallout 76, make sure the custom INI exists.
         context.api.events.on('gamemode-activated', (gameId) => onGameModeActivated(gameId, context.api));
 
-        // When deploying, build the list of BA2s
-        context.api.events.on('did-deploy', (profileId) => updateArchiveList(profileId, context.api));
+        // On profile state change update the list of BA2s
+        context.api.onStateChange(['persistent', 'profiles'],
+            (previous, current) => {
+                clearTimeout(debouceUpdate)
+                debouceUpdate = setTimeout(() => {
+                    deboucedUpdateArchiveList(previous, current, context.api)
+                }, 500);
+            });
     });
 
     return true;
@@ -213,24 +233,24 @@ function migrate200(api, oldVersion) {
 
     // Wait for the UI to load.
     return api.awaitUI()
-    .then(() => {
-        const modArray = Object.keys(mods).map(k => mods[k]);
-        // Interate through each mod to check for BA2 archives.
-        return Promise.all(modArray.map(mod => {
-            return new Promise((resolve, reject) => {
-                const modPath = path.join(stagingFolder, mod.installationPath);
-                let ba2archives = [];
-                // Walk through all files for this mod, adding BA2s to the array.
-                util.walk(modPath, (iterPath, stats) => {
-                    if (path.extname(iterPath) === ARCHIVE_EXT) {
-                        return ba2archives.push(path.basename(iterPath));
-                    }
-                })
-                // If we collected some BA2s, set the mod attribute.
-                .then(() => ba2archives.length ? api.store.dispatch(actions.setModAttribute(GAME_ID, mod.id, 'ba2archives', ba2archives)) : undefined)
-            });
-        }))
-    })
+        .then(() => {
+            const modArray = Object.keys(mods).map(k => mods[k]);
+            // Interate through each mod to check for BA2 archives.
+            return Promise.all(modArray.map(mod => {
+                return new Promise((resolve, reject) => {
+                    const modPath = path.join(stagingFolder, mod.installationPath);
+                    let ba2archives = [];
+                    // Walk through all files for this mod, adding BA2s to the array.
+                    util.walk(modPath, (iterPath, stats) => {
+                        if (path.extname(iterPath) === ARCHIVE_EXT) {
+                            return ba2archives.push(path.basename(iterPath));
+                        }
+                    })
+                        // If we collected some BA2s, set the mod attribute.
+                        .then(() => ba2archives.length ? api.store.dispatch(actions.setModAttribute(GAME_ID, mod.id, 'ba2archives', ba2archives)) : undefined)
+                });
+            }))
+        })
 
 }
 
